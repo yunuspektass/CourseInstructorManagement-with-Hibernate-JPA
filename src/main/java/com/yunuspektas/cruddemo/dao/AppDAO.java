684bf5c2d9fc2c51ce345c370a28754203902b67
Yunus Pektaş <145374270+yunuspektass@users.noreply.github.com>
2024-05-08 01:29:52 +0300

    CourseInstructorManagement with Hibernate JPA
    CourseInstructorManagement simplifies course, instructor, and student management with CRUD operations and relationships. Many-to-Many and Many-to-One relationships are utilized. Technologies: Spring Boot for web development, Hibernate/JPA for database operations, MySQL as the database, and RESTful API for client communication.

---
+++ src/main/java/com/yunuspektas/cruddemo/dao/AppDAO.java
@@ -0,0 +1,48 @@
+package com.yunuspektas.cruddemo.dao;
+
+import com.yunuspektas.cruddemo.entity.Course;
+import com.yunuspektas.cruddemo.entity.Instructor;
+import com.yunuspektas.cruddemo.entity.InstructorDetail;
+import com.yunuspektas.cruddemo.entity.Student;
+
+import java.util.List;
+
+public interface AppDAO {
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+
+    List<Course> findCoursesByInstructorId(int theId);
+
+    Instructor findInstructorByIdJoinFetch(int theId);
+
+    void update(Instructor tempInstructor);
+
+    void update(Course tempCourse);
+
+    Course findCourseById(int theId);
+
+    void deleteCourseById(int theId);
+
+    void save(Course theCourse);
+
+    Course findCourseAndReviewsByCourseId(int theId);
+
+    Course findCourseAndStudentsByCourseId(int theId);
+
+    Student findStudentAndCoursesByStudentId(int theId);
+
+    void update(Student tempStudent);
+
+    void deleteStudentById(int theId);
+}
+
+
+
+
